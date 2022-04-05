47d0d7692b9d81157671db86b4a4c6ce6b18794b
uberkeil <kyle.p.simpson@gmail.com>
2022-04-05 21:45:20 +1200

    commited directory, added gitignore

---
+++ README.md
@@ -0,0 +1,5 @@
+# My Resume...
+
+...Was made in R using the pagedown and datadrivencv packages.
+
+
---
+++ Rproj_files/common_variables.R
@@ -0,0 +1,17 @@
+# Common non-csv data between CV and resume
+
+# Header with description of what is trying to be accomplished with this CV/Resume
+intro_text <- "Driven and inquisitive self starter, with a strong administrative acumen and thorough knowledge of computer programming languages for data analysis, warehousing, and visualization.  Skilled facilitator & capable communicator experienced in qualitative & quantitative research methods.  
+I am currently seeking a Research Assistant position which would allow me to apply and further develop these skills."
+
+# Language skills for skill bars visualization
+skills <- tribble(
+  ~skill,               ~level,
+  "SPSS",               4,
+  "Excel",              4.75,
+  "SQL",                4,
+  "R",                  4,
+  "Bash",               3.5,
+  "Python",             2.75
+)
+
