e6b31aba6e84e595799348a1cca1fde8e0bffd2b
uberkeil <kyle.p.simpson@gmail.com>
2022-08-01 21:26:42 -0300

    to right changed to 90deg in build skillbar function

--- R/parsing_functions.R
+++ R/parsing_functions.R
@@ -97,7 +97,7 @@ build_skill_bars <- function(skills, out_of = 5, bar_color = "#66AED7", bar_back
   skills %>%
     mutate(width_percent = round(100*as.numeric(level)/out_of)) %>%
     glue_data("<div class = 'skill-bar'", 
-              "style = \"background:linear-gradient(to right,", 
+              "style = \"background:linear-gradient(90deg,", 
               "{bar_color} {width_percent}%,", 
               "{bar_background} {width_percent}%)\" >", 
               "{skill}", 
