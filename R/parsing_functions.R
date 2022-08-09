378434f1033bdccc34340652f2f86f2dc480efe5
uberkeil <kyle.p.simpson@gmail.com>
2022-08-09 12:37:40 -0300

    Update of position descriptions, gitignore, skillbar, address, summary statement, page formatting, file structure.

--- R/parsing_functions.R
+++ R/parsing_functions.R
@@ -100,9 +100,7 @@ build_skill_bars <- function(skills, out_of = 5){
     mutate(width_percent = round(100*level/out_of)) %>% 
     glue_data(
       "<div class = 'skill-bar'",
-      "style = \"background:linear-gradient(to right,",
-      "{bar_color} {width_percent}%,",
-      "{bar_background} {width_percent}% 100%)\" >",
+      "style = \"background:linear-gradient(to right,", "{bar_color} {width_percent}%,", "{bar_background} {width_percent}% 100%)\" >",
       "{skill}",
       "</div>"
     )
