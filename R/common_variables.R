5551907d82a5120043d6211dacda42b7a6a43e09
uberkeil <kyle.p.simpson@gmail.com>
2024-01-18 12:20:15 -0500

    Change of personal title and font sizing

--- R/common_variables.R
+++ R/common_variables.R
@@ -1,7 +1,7 @@
 # Common non-csv data between CV and resume
 
 # Header with description of what is trying to be accomplished with this CV/Resume
-my_title <- glue("Data Entry Specialist &nbsp; | &nbsp;  Office Administrator &nbsp; | &nbsp; Data Analyst")
+my_title <- glue("Addministrative Support Clerk &nbsp; | &nbsp;  Reporting Analyst &nbsp; | &nbsp; Data Entry Specialist")
 
 intro_text <- glue("With over 15 years in customer-facing roles and 5+ years specializing in data entry and analytics, I confidently bridge the gap between interpersonal, analytical, and technical skills. A driven and inquisitive self-starter with strong administrative acumen, and fluency in a vast suite of tools particular to office administration, data analysis, and customer service/relations.")
 
